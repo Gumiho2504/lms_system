886b47b2f3fdc9632fa813dd8e3c19fbeb4ca88e
Gumiho2504 <hemchanmetrey123@gmail.com>
2025-01-31 13:08:42 +0700

    admin_middleware

--- resources/views/layouts/admin-app.blade.php
+++ resources/views/layouts/admin-app.blade.php
@@ -16,7 +16,7 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+            @include('layouts.admin-navigation')
 
             <!-- Page Heading -->
             @isset($header)
@@ -28,8 +28,8 @@
             @endisset
 
             <!-- Page Content -->
-            {{ $slot }}
             <main>
+                {{ $slot }}
             </main>
         </div>
     </body>
--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,9 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::get('/admin/dashboard', function () {
+    return view('admin.dashboard');
+})->middleware(['auth:admin', 'verified'])->name('admin.dashboard');
+require __DIR__ . '/admin.php';
+require __DIR__ . '/auth.php';
+
